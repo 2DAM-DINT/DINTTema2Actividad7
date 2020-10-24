1a6fb0853b155e5a70d9cabf63930efa122fa76b
adrianCG <adrianCG@mesi>
2020-10-24 10:53:26 +0200

    Subo el proyecto

---
+++ Actividad7/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Actividad7
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            pequeño.IsChecked = true;
+        }
+
+        private void radioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            texto.FontSize = 
+                double.Parse((sender as RadioButton).Tag.ToString());
+        }
+    }
+}
